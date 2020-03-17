d808e84c311743ddb91582844ceaab9fd62aed8c
Yusuf Simonson <simonson@gmail.com>
2020-03-16 22:23:35 -0400

    Pachyderm shell

--- images/user/pachyderm-jupyterlab-extension/src/index.ts
+++ images/user/pachyderm-jupyterlab-extension/src/index.ts
@@ -3,7 +3,9 @@ import {
 } from '@jupyterlab/application';
 import { IMainMenu } from '@jupyterlab/mainmenu';
 import { Menu } from '@lumino/widgets';
-import { ICommandPalette } from '@jupyterlab/apputils';
+import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
+import { Terminal } from '@jupyterlab/terminal/lib/widget';
+import { terminalIcon } from '@jupyterlab/ui-components';
 
 import { DAGWidget } from './dag';
 
@@ -17,33 +19,56 @@ const extension: JupyterFrontEndPlugin<void> = {
   activate: (app: JupyterFrontEnd, palette: ICommandPalette, mainMenu: IMainMenu) => {
     console.log('pachyderm-jupyterlab-extension is activated!');
 
-    const { commands } = app;
+    const addAction = (command, label, executeFn) => {
+      // Add the command
+      commands.addCommand(command, {
+        label: label,
+        caption: 'Execute ' + command + ' Command',
+        execute: executeFn
+      });
 
-    // DAG command
-    const command = 'pachyderm:dag-viewer';
-    commands.addCommand(command, {
-      label: 'DAG',
-      caption: 'Execute pachyderm:dag-viewer Command',
-      execute: (args) => {
-        const widget = new DAGWidget();
-        app.shell.add(widget, 'main');
-      }
-    });
+      // Add to command palette
+      palette.addItem({
+        command,
+        category: 'Pachyderm',
+        args: { origin: 'palette' }
+      });
 
-    // Add the command to the command palette
-    palette.addItem({
-      command,
-      category: 'Pachyderm',
-      args: { origin: 'palette' }
-    });
+      // Add to the menu
+      pachydermMenu.addItem({ command, args: { origin: 'menu' } });
+    };
 
     // Create a menu
+    const { commands, serviceManager } = app;
     const pachydermMenu: Menu = new Menu({ commands });
     pachydermMenu.title.label = 'Pachyderm';
     mainMenu.addMenu(pachydermMenu, { rank: 80 });
 
-    // Add the command to the menu
-    pachydermMenu.addItem({ command, args: { origin: 'menu' } });
+    addAction('pachyderm:dag-viewer', 'DAG', (args) => {
+      const widget = new DAGWidget();
+      app.shell.add(widget, 'main');
+    });
+    
+    addAction('pachyderm:shell', 'Shell', async (args) => {
+      // adapted from
+      // https://github.com/jupyterlab/jupyterlab/blob/master/packages/terminal-extension/src/index.ts
+      const name = args['name'] as string;
+
+      const session = await (name
+        ? serviceManager.terminals.connectTo({ model: { name } })
+        : serviceManager.terminals.startNew());
+
+      const term = new Terminal(session, {
+        initialCommand: "pachctl shell"
+      });
+
+      term.title.icon = terminalIcon;
+      term.title.label = 'pachyderm shell';
+
+      let widget = new MainAreaWidget({ content: term });
+      app.shell.add(widget);
+      app.shell.activateById(widget.id);
+    });
   }
 };
 
