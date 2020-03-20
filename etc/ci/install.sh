c7b7d4a8c62d37fffe879705fcf6b0565ad327e9
Yusuf Simonson <simonson@gmail.com>
2020-03-20 13:07:04 -0400

    Always get the same version of pachctl

--- etc/ci/install.sh
+++ etc/ci/install.sh
@@ -16,15 +16,16 @@ pip3 install --upgrade --user wheel
 pip3 install --upgrade --user awscli
 
 # Install pachctl
-curl -o /tmp/pachctl.deb -L https://github.com/pachyderm/pachyderm/releases/download/v$(PACHYDERM_VERSION)/pachctl_$(PACHYDERM_VERSION)_amd64.deb  && \
+pachyderm_version=$(jq -r .pachctl < version.json)
+curl -o /tmp/pachctl.deb -L https://github.com/pachyderm/pachyderm/releases/download/v$(pachyderm_version)/pachctl_$(pachyderm_version)_amd64.deb  && \
 sudo dpkg -i /tmp/pachctl.deb
 
 # Install kubectl
 # To get the latest kubectl version:
 # curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt
 if [ ! -f ~/cached-deps/kubectl ] ; then
-    KUBECTL_VERSION=v1.13.0
-    curl -L -o kubectl https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl && \
+    kubectl_version=v1.13.0
+    curl -L -o kubectl https://storage.googleapis.com/kubernetes-release/release/${kubectl_version}/bin/linux/amd64/kubectl && \
         chmod +x ./kubectl && \
         mv ./kubectl ~/cached-deps/kubectl
 fi
@@ -33,8 +34,8 @@ fi
 # To get the latest minikube version:
 # curl https://api.github.com/repos/kubernetes/minikube/releases | jq -r .[].tag_name | sort | tail -n1
 if [ ! -f ~/cached-deps/minikube ] ; then
-    MINIKUBE_VERSION=v0.31.0
-    curl -L -o minikube https://storage.googleapis.com/minikube/releases/${MINIKUBE_VERSION}/minikube-linux-amd64 && \
+    minikube_version=v0.31.0
+    curl -L -o minikube https://storage.googleapis.com/minikube/releases/${minikube_version}/minikube-linux-amd64 && \
         chmod +x ./minikube && \
         mv ./minikube ~/cached-deps/minikube
 fi
