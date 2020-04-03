31165d0fff725cbb1699e6a144b6dc23170b5ee9
Yusuf Simonson <simonson@gmail.com>
2020-04-03 16:34:15 -0400

    Try cleaning up output

--- etc/ci/test.sh
+++ etc/ci/test.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-set -ex
+set -e
+
+color_red='\033[0;31m'
+color_none='\033[0m'
+
+function print_section {
+    echo -e "I ${color_red}${1}${color_none}"
+}
 
 # Deploys pachyderm and activates enterprise+auth
 function deploy_pachyderm {
@@ -24,92 +31,78 @@ function test_run {
     # TODO: run through testing the login process via selenium/firefox
 }
 
-./etc/ci/start_minikube.sh
-
-pushd images/hub
-    make docker-build
-popd
-
-pushd images/user
-    make docker-build
-popd
-
-image_version=$(jq -r .jupyterhub_pachyderm < version.json)
-./etc/ci/push-to-minikube.sh pachyderm/jupyterhub-pachyderm-hub:${image_version}
-./etc/ci/push-to-minikube.sh pachyderm/jupyterhub-pachyderm-user:${image_version}
-
 case "${VARIANT}" in
     native)
-        # Deploy pachyderm
+        print_section "Deploy pachyderm"
         deploy_pachyderm
 
-        # Deploy jupyterhub
+        print_section "Deploy jupyterhub"
         ${GOPATH}/bin/pachctl deploy jupyterhub \
             --user-image pachyderm/jupyterhub-pachyderm-user:${image_version} \
             --hub-image pachyderm/jupyterhub-pachyderm-hub:${image_version}
         test_run
 
-        # Upgrade jupyterhub
+        print_section "Upgrade jupyterhub"
         ${GOPATH}/bin/pachctl deploy jupyterhub \
             --user-image pachyderm/jupyterhub-pachyderm-user:${image_version} \
             --hub-image pachyderm/jupyterhub-pachyderm-hub:${image_version}
         test_run
 
-        # Undeploy
+        print_section "Undeploy"
         echo yes | ${GOPATH}/bin/pachctl undeploy --jupyterhub --metadata
 
-        # Reset minikube
+        print_section "Reset minikube"
         minikube delete
         ./etc/ci/start_minikube.sh
 
-        # Re-deploy pachyderm
+        print_section "Re-deploy pachyderm"
         deploy_pachyderm
 
-        # Re-deploy jupyterhub
+        print_section "Re-deploy jupyterhub"
         ${GOPATH}/bin/pachctl deploy jupyterhub \
             --user-image pachyderm/jupyterhub-pachyderm-user:${image_version} \
             --hub-image pachyderm/jupyterhub-pachyderm-hub:${image_version}
         test_run
         ;;
     python)
-        # Deploy pachyderm
+        print_section "Deploy pachyderm"
         deploy_pachyderm
 
-        # Deploy jupyterhub
+        print_section "Deploy jupyterhub"
         python3.7 init.py
         test_run
 
-        # Upgrade jupyterhub
+        print_section "Upgrade jupyterhub"
         python3.7 init.py
         test_run
 
-        # Undeploy
+        print_section "Undeploy"
         ./delete.sh
 
-        # Reset minikube and hostpaths
+        print_section "Reset minikube and hostpaths"
         minikube delete
         sudo rm -rf /var/pachyderm
         ./etc/ci/start_minikube.sh
 
-        # Re-deploy pachyderm
+        print_section "Re-deploy pachyderm"
         deploy_pachyderm
 
-        # Re-deploy jupyterhub
+        print_section "Re-deploy jupyterhub"
         python3.7 init.py
         test_run
         ;;
     existing)
-        # Create a base deployment of jupyterhub
+        print_section "Create a base deployment of jupyterhub"
         python3 ./etc/ci/existing_config.py base \
             | helm upgrade --install jhub jupyterhub/jupyterhub --version 0.8.2 --values -
         wait_for jupyterhub
 
-        # Patch in the user image
+        print_section "Patch in the user image"
         python3 ./etc/ci/existing_config.py patch \
             | helm upgrade jhub jupyterhub/jupyterhub --version 0.8.2 --values -
         test_run
 
-        # Undeploy
+        print_section "Undeploy"
         ./delete.sh
         ;;
     *)
