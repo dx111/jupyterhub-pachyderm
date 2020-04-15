898afcfc21c525fd3d8e7aa7c0c5f25bf7fbd963
Yusuf Simonson <simonson@gmail.com>
2020-04-15 13:00:08 -0400

    Only use none driver on travis

--- etc/start_minikube.sh
+++ etc/start_minikube.sh
@@ -2,28 +2,28 @@
 
 set -ex
 
-minikube_args=(
-    "--vm-driver=none"
-)
+if [ "$TRAVIS" = "true" ]; then
+    # Repeatedly restart minikube until it comes up. This corrects for an issue in
+    # Travis, where minikube will get stuck on startup and never recover
+    while true; do
+        sudo env "PATH=$PATH" "CHANGE_MINIKUBE_NONE_USER=true" minikube start -vm-driver=none
+        HEALTHY=false
+        # Try to connect for one minute
+        for _ in $(seq 12); do
+            if {
+                kubectl version 2>/dev/null >/dev/null
+            }; then
+                HEALTHY=true
+                break
+            fi
+            sleep 5
+        done
+        if [ "${HEALTHY}" = "true" ]; then break; fi
 
-# Repeatedly restart minikube until it comes up. This corrects for an issue in
-# Travis, where minikube will get stuck on startup and never recover
-while true; do
-    sudo env "PATH=$PATH" "CHANGE_MINIKUBE_NONE_USER=true" minikube start "${minikube_args[@]}"
-    HEALTHY=false
-    # Try to connect for one minute
-    for _ in $(seq 12); do
-        if {
-            kubectl version 2>/dev/null >/dev/null
-        }; then
-            HEALTHY=true
-            break
-        fi
-        sleep 5
+        # Give up--kubernetes isn't coming up
+        minikube delete
+        sleep 10 # Wait for minikube to go completely down
     done
-    if [ "${HEALTHY}" = "true" ]; then break; fi
-
-    # Give up--kubernetes isn't coming up
-    minikube delete
-    sleep 10 # Wait for minikube to go completely down
-done
+else
+    minikube start
+fi
