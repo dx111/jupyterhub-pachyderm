4d07723692a31bba9704ceebed498ca58a9b2c19
Yusuf Simonson <simonson@gmail.com>
2020-04-15 13:01:30 -0400

    Revert "Don't push to minikube, since we're using the none driver"
    This reverts commit 26030ad3d4b51abd32d3cca294007c092d4568a8.

--- etc/travis_setup.sh
+++ etc/travis_setup.sh
@@ -14,3 +14,7 @@ popd
 pushd images/user
     make docker-build
 popd
+
+image_version=$(jq -r .jupyterhub_pachyderm < version.json)
+./etc/push-to-minikube.sh pachyderm/jupyterhub-pachyderm-hub:${image_version}
+./etc/push-to-minikube.sh pachyderm/jupyterhub-pachyderm-user:${image_version}
