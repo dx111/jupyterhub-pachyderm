d35e937a480f42bfffef5047bafcb02385079e95
Yusuf Simonson <simonson@gmail.com>
2020-04-15 12:01:46 -0400

    Use latest minikube/kubectl

--- etc/travis_install.sh
+++ etc/travis_install.sh
@@ -22,7 +22,7 @@ docker tag pachyderm/worker:${pachyderm_version} pachyderm/worker:local
 # To get the latest kubectl version:
 # curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt
 if [ ! -f ~/cached-deps/kubectl ] ; then
-    kubectl_version=v1.13.0
+    kubectl_version=v1.18.1
     curl -L -o kubectl https://storage.googleapis.com/kubernetes-release/release/${kubectl_version}/bin/linux/amd64/kubectl && \
         chmod +x ./kubectl && \
         mv ./kubectl ~/cached-deps/kubectl
@@ -30,9 +30,9 @@ fi
 
 # Install minikube
 # To get the latest minikube version:
-# curl https://api.github.com/repos/kubernetes/minikube/releases | jq -r .[].tag_name | sort | tail -n1
+# curl https://api.github.com/repos/kubernetes/minikube/releases | jq -r '.[].tag_name' | sort | tail -n1
 if [ ! -f ~/cached-deps/minikube ] ; then
-    minikube_version=v0.31.0
+    minikube_version=v1.9.2
     curl -L -o minikube https://storage.googleapis.com/minikube/releases/${minikube_version}/minikube-linux-amd64 && \
         chmod +x ./minikube && \
         mv ./minikube ~/cached-deps/minikube
